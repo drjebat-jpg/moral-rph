ac25c2b5fc0f2527b845635b4326bd555d9d8988
Ts.Jebat <drjebat@gmail.com>
2026-03-14 12:54:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Tambahkan baris ini supaya Cloudflare jumpa fail aset
+})import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
